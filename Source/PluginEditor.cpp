8c2c3f0f46583ad547e48fc0c8f5d73d0f92c666
tomteschhowest <tom.tesch@howest.be>
2023-04-20 09:18:47 +0200

    changed the message

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -30,7 +30,7 @@ void Demo2AudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("Hello ISP!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void Demo2AudioProcessorEditor::resized()
